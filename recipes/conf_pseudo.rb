c274be850ac3af64283741e79b73647061022edb
Michael Dellanoce <michael.dellanoce@gmail.com>
2013-04-01 10:03:52 -0400

    update conf_pseudo recipe to CDH4

--- recipes/conf_pseudo.rb
+++ recipes/conf_pseudo.rb
@@ -16,14 +16,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# Instructions:
+# https://ccp.cloudera.com/display/CDH4DOC/Deploying+MapReduce+v1+%28MRv1%29+on+a+Cluster
 
 include_recipe "hadoop"
 
-package "hadoop-conf-pseudo"
+package "hadoop-0.20-conf-pseudo"
 
-%w{namenode secondarynamenode datanode jobtracker tasktracker}.each do |d|
+hdfs = "hdfs"
+
+execute "hdfs namenode -format -nonInteractive" do
+  action :run
+  user hdfs
+  returns [0,1]
+end
+
+%w{hdfs-namenode hdfs-secondarynamenode hdfs-datanode}.each do |d|
   service "hadoop-#{d}" do
     action [ :start, :enable ]
   end
 end
 
+mapred_cache_dir = "/var/lib/hadoop-hdfs/cache/mapred"
+mapred_system_dir = "/tmp/mapred/system"
+commands = [
+  "hadoop fs -mkdir -p /tmp",
+  "hadoop fs -mkdir -p #{mapred_cache_dir}/staging",
+  "hadoop fs -chmod 1777 #{mapred_cache_dir}/staging",
+  "hadoop fs -chown -R mapred #{mapred_cache_dir}",
+  "hadoop fs -mkdir -p #{mapred_system_dir}",
+  "hadoop fs -chown mapred:hadoop #{mapred_system_dir}"
+]
+
+commands.each do |command|
+  execute command do
+    action :run
+    user hdfs
+  end
+end
+
+%w{jobtracker tasktracker}.each do |d|
+  service "hadoop-0.20-mapreduce-#{d}" do
+    action [ :start, :enable ]
+  end
+end
